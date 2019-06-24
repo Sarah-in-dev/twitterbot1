c582e6d8aca732d082a05b0ae39cc6574d71e93a
Sarah-in-dev <50673314+Sarah-in-dev@users.noreply.github.com>
2019-06-24 16:55:52 -0400

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key: 'RdIbxnWB9zlHyVGdkHjSmrzVY',
-  consumer_secret: 'eSkKBnfBKClnKOhiMRV83LgYRBfXJCcZgWiGkLIqIyybVhcjO8',
-  access_token_key: '1000534287517650951-hjQkhra7Zvn8gWtjXTy7wRsq5UendA',
-  access_token_secret: 'jdonnvmDZtgNhzFMPtzFCzVTbyDKM7HRDP8XiEGzIf3vj'
-}
